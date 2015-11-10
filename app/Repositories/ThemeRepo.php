0d5364c9f124824f51673aaebc4b0cd4f6cb5b7d
Oscar Guevara <yhonguevara@gmail.com>
2015-11-09 22:51:31 -0430

    Order by name theme in categores

--- app/Repositories/ThemeRepo.php
+++ app/Repositories/ThemeRepo.php
@@ -13,7 +13,7 @@ class ThemeRepo extends BaseRepo
 
   public function getListThemes()
   {
-    return \DB::select('SELECT t.id, t.category_id, t.name FROM themes t INNER JOIN categories c ON (c.id = t.category_id) ORDER BY c.name');
+    return \DB::select('SELECT t.id, t.category_id, t.name FROM themes t INNER JOIN categories c ON (c.id = t.category_id) ORDER BY c.name, t.name');
     // return Theme::orderby('category_id', 'ASC')->get();
   }
 
