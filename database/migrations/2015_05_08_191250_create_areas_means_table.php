cb8e8c8c46674e458d411c319d4f48ad4e2547cf
yhon92 <yhonguevara@gmail.com>
2015-11-12 14:53:34 -0430

    refractor name means_areas for areas_means

--- database/migrations/2015_05_08_191250_create_areas_means_table.php
+++ database/migrations/2015_05_08_191250_create_areas_means_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMeansAreasTable extends Migration
+class CreateAreasMeansTable extends Migration
 {
     /**
      * Run the migrations.
