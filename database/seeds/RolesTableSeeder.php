3cc59afeaac8652fb2ecbb2b2e2c1c008b5f2da1
yhon92 <yhonguevara@gmail.com>
2016-01-05 15:32:10 -0430

    Create branch develop

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Johnnymn\Sim\Roles\Models\Role;
+
+class RolesTableSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+
+    $adminRole = Role::create([
+      'name' => 'Administrador',
+      'slug' => 'admin',
+      'description' => '',
+    ]);
+
+    $coordinatorRole = Role::create([
+      'name' => 'Coordinador',
+      'slug' => 'coordinator',
+      'description' => '',
+    ]);
+
+    $moderatorRole = Role::create([
+      'name' => 'Moderador',
+      'slug' => 'moderator',
+      'description' => '',
+    ]);
+
+    $guestRole = Role::create([
+      'name' => 'Invitado',
+      'slug' => 'guest',
+      'description' => '',
+    ]);
+  }
+}
